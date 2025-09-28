111230994bb7e0869aa94eff89bd5718c20de11a
Aryan <aryan.project22@gmail.com>
2025-09-28 23:46:49 +0530

    naresh push v8

--- index.js
+++ index.js
@@ -92,6 +92,20 @@ const upload = multer({
   }
 });
 
+// CORS middleware to allow all origins
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
